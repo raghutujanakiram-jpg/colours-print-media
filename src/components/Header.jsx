55061f5d191078d516cc5ef6b83b0f9f134ac7d0
Janakiram <raghutujanakiram@gmail.com>
2025-12-16 20:26:07 +0530

    style: global layout, header, typography polish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,19 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="logo">Colours Print Media</div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/mission-vision">Mission & Vision</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/clients">Clients</Link>
-        <Link to="/testimonials">Testimonials</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
+    <header className="site-header">
+      <div className="header-inner">
+        <div className="logo">Colours Print Media</div>
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/mission-vision">Mission & Vision</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/clients">Clients</Link>
+          <Link to="/testimonials">Testimonials</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
     </header>
   );
 }
